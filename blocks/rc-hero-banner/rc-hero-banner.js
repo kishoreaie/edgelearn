8128a1004f44f2c7a35e3fa67babf9c3c8a9b0a1
kishore kumar <kmurugan@aienterprise.com>
2025-01-19 14:12:30 +0530

    Aded new change

--- blocks/rc-hero-banner/rc-hero-banner.js
+++ blocks/rc-hero-banner/rc-hero-banner.js
@@ -25,6 +25,9 @@ export default function decorate(block) {
     const recipeDetailDietary = data.recipeDetailDietary;
     const recipeDetailSubmittedBy = data.recipeDetailSubmittedBy;
   
+    // Clear block to avoid appending multiple times
+    block.innerHTML = '';
+  
     // Create Hero Area
     const heroArea = document.createElement('div');
     heroArea.classList.add('ognm-header-recipe__hero-area', 'clrs-dark', 'clrs-primary');
@@ -33,16 +36,17 @@ export default function decorate(block) {
     heroBg.classList.add('ognm-header-recipe__hero-area__bg');
     const picture = document.createElement('picture');
     picture.classList.add('ognm-header-recipe__hero-area__bg__picture');
+  
     if (desktopImage) {
       const sourceDesktop = document.createElement('source');
       sourceDesktop.media = '(min-width: 1025px)';
-      sourceDesktop.srcset = `${desktopImage}`;
+      sourceDesktop.srcset = desktopImage;
       picture.appendChild(sourceDesktop);
     }
     if (mobileImage) {
       const sourceMobile = document.createElement('source');
       sourceMobile.media = '(min-width: 768px)';
-      sourceMobile.srcset = `${mobileImage}`;
+      sourceMobile.srcset = mobileImage;
       picture.appendChild(sourceMobile);
     }
     const img = document.createElement('img');
@@ -57,26 +61,31 @@ export default function decorate(block) {
     heroCaption.classList.add('ognm-header-recipe__hero-area__caption', 'elmt-caption');
     const captionInner = document.createElement('div');
     captionInner.classList.add('elmt-caption__inner');
+  
     if (eyebrow) {
       const eyebrowElement = document.createElement('p');
       eyebrowElement.classList.add('elmt-caption__eyebrow', 'atom-eyebrow', 'atom-eyebrow--mega');
       eyebrowElement.textContent = eyebrow;
       captionInner.appendChild(eyebrowElement);
     }
+  
     if (heading) {
       const headingElement = document.createElement('h1');
       headingElement.classList.add('elmt-caption__title', 'atom-heading', 'atom-heading--lrg');
       headingElement.textContent = heading;
       captionInner.appendChild(headingElement);
     }
+  
     const ctas = document.createElement('div');
     ctas.classList.add('elmt-caption__ctas', 'elmt-buttons');
+  
     if (saveRecipeText) {
       const saveRecipeButton = document.createElement('button');
       saveRecipeButton.classList.add('atom-button');
       saveRecipeButton.textContent = saveRecipeText;
       ctas.appendChild(saveRecipeButton);
     }
+  
     if (jumpRecipeText) {
       const jumpRecipeLink = document.createElement('a');
       jumpRecipeLink.classList.add('atom-button-link', 'js-jump-link');
@@ -84,9 +93,11 @@ export default function decorate(block) {
       jumpRecipeLink.textContent = jumpRecipeText;
       ctas.appendChild(jumpRecipeLink);
     }
+  
     captionInner.appendChild(ctas);
     heroCaption.appendChild(captionInner);
     heroArea.appendChild(heroCaption);
+  
     block.appendChild(heroArea);
   
     // Author Card
@@ -95,6 +106,7 @@ export default function decorate(block) {
       authorCard.classList.add('ognm-header-recipe__author-card');
       const authorCardInner = document.createElement('div');
       authorCardInner.classList.add('ognm-header-recipe__author-card__card', 'clrs-light', 'clrs-primary');
+  
       const authorCaption = document.createElement('div');
       authorCaption.classList.add('elmt-caption');
       const authorCaptionInner = document.createElement('div');
@@ -106,39 +118,42 @@ export default function decorate(block) {
         authorEyebrowElement.textContent = authorCardEyebrow;
         authorCaptionInner.appendChild(authorEyebrowElement);
       }
+  
       if (authorCardHeading) {
         const authorHeadingElement = document.createElement('h2');
         authorHeadingElement.classList.add('elmt-caption__title', 'atom-heading', 'atom-heading--sub-med');
         authorHeadingElement.textContent = authorCardHeading;
         authorCaptionInner.appendChild(authorHeadingElement);
       }
+  
       const authorDetails = document.createElement('div');
       authorDetails.classList.add('elmt-caption__author');
-      const authorInfo = document.createElement('div');
-      authorInfo.classList.add('elmt-author');
   
       if (authorImage) {
         const authorImg = document.createElement('img');
         authorImg.classList.add('elmt-author__portrait__image');
         authorImg.src = authorImage;
         authorImg.alt = 'Author Image';
-        authorInfo.appendChild(authorImg);
+        authorDetails.appendChild(authorImg);
       }
-      const authorText = document.createElement('div');
-      authorText.classList.add('elmt-author__info');
+  
+      const authorInfo = document.createElement('div');
+      authorInfo.classList.add('elmt-author__info');
+  
       if (authorName) {
         const authorNameElement = document.createElement('div');
         authorNameElement.classList.add('elmt-author__info__name');
         authorNameElement.textContent = authorName;
-        authorText.appendChild(authorNameElement);
+        authorInfo.appendChild(authorNameElement);
       }
+  
       if (authorDesignation) {
         const authorDesignationElement = document.createElement('div');
         authorDesignationElement.classList.add('elmt-author__info__desc');
         authorDesignationElement.textContent = authorDesignation;
-        authorText.appendChild(authorDesignationElement);
+        authorInfo.appendChild(authorDesignationElement);
       }
-      authorInfo.appendChild(authorText);
+  
       authorDetails.appendChild(authorInfo);
       authorCaptionInner.appendChild(authorDetails);
       authorCaption.appendChild(authorCaptionInner);
@@ -148,22 +163,29 @@ export default function decorate(block) {
     }
   
     // Recipe Details
-    const recipeDetail = document.createElement('div');
-    recipeDetail.classList.add('ognm-header-recipe__detail-area');
-    const detailContainer = document.createElement('div');
-    detailContainer.classList.add('ognm-header-recipe__container');
-    if (recipeDetailHeading) {
-      const recipeHeading = document.createElement('h2');
-      recipeHeading.textContent = recipeDetailHeading;
-      detailContainer.appendChild(recipeHeading);
+    if (recipeDetailHeading || recipeDetailDescription || recipeDetailTime || recipeDetailYield || recipeDetailDifficulty || recipeDetailDietary || recipeDetailSubmittedBy) {
+      const recipeDetail = document.createElement('div');
+      recipeDetail.classList.add('ognm-header-recipe__detail-area');
+  
+      const detailContainer = document.createElement('div');
+      detailContainer.classList.add('ognm-header-recipe__container');
+  
+      if (recipeDetailHeading) {
+        const recipeHeading = document.createElement('h2');
+        recipeHeading.classList.add('atom-heading');
+        recipeHeading.textContent = recipeDetailHeading;
+        detailContainer.appendChild(recipeHeading);
+      }
+  
+      if (recipeDetailDescription) {
+        const recipeDescription = document.createElement('p');
+        recipeDescription.classList.add('atom-text');
+        recipeDescription.innerHTML = recipeDetailDescription;
+        detailContainer.appendChild(recipeDescription);
+      }
+  
+      recipeDetail.appendChild(detailContainer);
+      block.appendChild(recipeDetail);
     }
-    if (recipeDetailDescription) {
-      const recipeDescription = document.createElement('p');
-      recipeDescription.innerHTML = recipeDetailDescription;
-      detailContainer.appendChild(recipeDescription);
-    }
-    recipeDetail.appendChild(detailContainer);
-    block.appendChild(recipeDetail);
 
 }
-  
